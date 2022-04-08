323b9ea351f4aefb344df5d8998d986168b186e1
otazero <zero.kotam@gmail.com>
2022-04-08 14:21:35 +0900

    基本の木

--- src-js/index.js
+++ src-js/index.js
@@ -1,21 +1,41 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const { Client } = require('discord.js')
-const token = process.env.BOT_TOKEN
+const { Client } = require('discord.js');
+const token = process.env.BOT_TOKEN;
 const client = new Client({
-  intents: [/** 必要に応じてIntentを追加すること */]
-})
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+});
 
 client.once('ready', () => {
-  console.log('Ready!')
-})
+  console.log('接続しました！');
+});
 
-client.on('messageCreate', (message) => {
-  console.log('通過！')
-  if (message.content === 'やぁ' && !message.author.bot){
-    message.reply('こんにちは！').catch(console.error)
+// client.on('message', (message) => {
+//   console.log('通過！');
+//   if (message.content === 'やぁ' && !message.author.bot){
+//     message.reply('こんにちは！').catch(console.error);
+//   }
+// });
+
+client.on("messageCreate", (message) => {
+  console.log("通過！");
+  if (message.author.bot) { //botからのmessageを無視
+    return;
   }
-})
+  let msg = message.content; //ユーザが送信したメッセージはmessage.contentで取得可能
+  message.channel.send(msg); //メッセージが送られたチャンネルに返信
+});
+// client.on('message', async message => {
+//   if (message.content === '!prompt') {
+//     message.channel.send('yes か no を送信してください')
+//     const filter = msg => msg.author.id === message.author.id
+//     const collected = await message.channel.awaitMessages({ filter, max: 1, time: 10000 })
+//     const response = collected.first()
+//     if (!response) return message.channel.send('タイムアウト')
+//     if (!['yes', 'no'].includes(response.content)) return message.channel.send('正しくありません')
+//     message.channel.send(`${response.content} が送信されました`)
+//   }
+// })
 
 client.login(token)
   .catch(console.error)
