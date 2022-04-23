22b5c02ce5a1f1b6f181739974718c87198041c7
otazero <zero.kotam@gmail.com>
2022-04-23 13:54:08 +0900

    毎日の惑星完了(仮)

--- src-js/src/process/main.js
+++ src-js/src/process/main.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2/promise");
 const scrape = require('./scrape');
+const  nowDateTime = require("./nowDateTime");
 require('dotenv').config();
 
 // メンバー一覧表示サイトURL
@@ -20,31 +21,41 @@ async function runEveryDay(){
     // TODO:wt_membersにいなければ追加、いれば放置
     const con = await mysql.createConnection(db_setting);
     const listLength = scList.length;
+    let scIgnList = [];
     for(let i = 1; i < listLength; i++){
         const [resultsWTign, field] = await con.query(`SELECT t_ign FROM t_wt_members WHERE t_ign = '${scList[i].player}' LIMIT 1`);
         if(!resultsWTign.length){
             // 基本情報
             await con.query(`INSERT INTO t_wt_members(t_ign, r_id, t_enter_at)VALUES ('${list[i].player}',${list[i].roleid},'${list[i].dateOfEntry}')`);
         }
+        scIgnList.push(scList[i].player);
     }
-    //await Promise.all([
-        // 基本情報
-    //    await con.query(`INSERT INTO t_wt_members(t_ign, r_id, t_enter_at, t_all_active)VALUES ${hairetu['info']}`),
-        // アクティブ情報
-    //    con.query(`INSERT INTO wt_actives(t_user_id, wt_active)VALUES ${hairetu['activity']}`)
-    //]);
     // TODO:wt_membersがスクレイピングにいなければ, wt_membersの在籍フラグを変更・退室日を記録
     const [rows, fields] = await con.query(`SELECT t_ign FROM t_wt_members`);
+    let byebyeWT = [];
+    const now = new nowDateTime();
+    for(let row of rows){
+        if(!scIgnList.includes(row.t_ign)){
+            byebyeWT.push(row.t_ign);
+            await con.query(`UPDATE t_wt_members SET t_is_flag = 0 , t_left_at = '${now.getDate()}' WHERE t_ign = '${row.t_ign}'`);
+        }
+    }
     await con.end();
-    // console.log(rows);
 };
 
 // スクレイピング 
 async function scraping(){
     const list = await scrape.fetch(clanInfoURL);
     return list;
-}
+};
 
+
+/*
+const now = new nowDateTime();
+
+console.log(now.getMonth());
+console.log(now.getDate());
+*/
 module.exports = {
     runEveryDay
 };
