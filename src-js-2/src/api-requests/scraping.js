313b05e268d0d56822468e05909859208cc4262c
Dorayaki-Chan <zero.kotam@gmail.com>
2022-06-15 15:41:07 +0900

    スクレイピングほぼほぼ完成

---
+++ src-js-2/src/api-requests/scraping.js
@@ -0,0 +1,64 @@
+const client = require("cheerio-httpcli");
+const fs = require('fs');
+const ini = require('ini');
+const { PassThrough } = require("stream");
+const {ThunderUser} = require('../structures/profile');
+const {shapDatetime} = require('../change-datetime-type/toDatetime');
+
+const config = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'));
+
+const URL = encodeURI(`https://warthunder.com/en/community/claninfo/${config.ThunderConfig.clanname}`);
+
+class Scrape{
+    static async thunderClanTable(){
+        const result = await client.fetch(URL);
+        const $ = result.$;
+        let users = [];
+        $('div.squadrons-members__grid-item').each((index, value)=>{
+            if(0 <= index && index < 6){
+                return;
+            }
+            else{
+                const i = Math.trunc(index / 6) - 1;
+                switch (index % 6) {
+                    case 0:
+                        users.push(new ThunderUser);
+                        break;
+                    case 1:
+                        //console.log(value.children[1].attribs.href.replace('en/community/userinfo/?nick=', ''));
+                        const ign = value.children[1].attribs.href.replace('en/community/userinfo/?nick=', '').split('@');
+                        users[i].ign = ign[0];
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        // console.log(value.children[0].data.replace(/\s+/g, ''));
+                        users[i].nowactive = Number(value.children[0].data.replace(/\s+/g, ''));
+                        break;
+                    case 4:
+                        //　console.log(value.children[0].data.replace(/\s+/g, ''));
+                        users[i].role = value.children[0].data.replace(/\s+/g, '');
+                        break;
+                    case 5:
+                        //console.log(value.children[0].data.replace(/\s+/g, ''));
+                        const day = new shapDatetime(value.children[0].data.replace(/\s+/g, ''));
+                        users[i].enter_at = day.getDate;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        console.log(users);
+        return users;
+    }
+}
+
+Scrape.thunderClanTable();
+
+
+
+module.exports = {
+    Scrape
+};
+
