aaf36101f79f69ab34ad939e27206670b4a9aa9d
Dorayaki-Chan <zero.kotam@gmail.com>
2022-06-16 13:53:44 +0900

    ロールクラス作成

--- src-js-2/src/what-your-Info/whatYourInfo.js
+++ src-js-2/src/what-your-Info/whatYourInfo.js
@@ -35,6 +35,32 @@ class WhatYourIgn{
         return ign;
     }
 }
+const roleData= require('../../template/roles.json');
+
+class WhatYourRole{
+    /**
+     * 
+     * @param {string[]} roles ロール名orロールID
+     */
+    constructor(roles){
+        const test = roles.map(role => {
+            const templi = roleData.map(roleinfo =>{
+                if(roleinfo.discordid === role){
+                    return roleinfo;
+                }
+                else{
+                    if(roleinfo.othername.some(other => other === role)){
+                        return roleinfo;
+                    }
+                }
+            }).filter(Boolean);
+            return templi[0];
+        });
+        console.log(test);
+    }
+}
+
+const test = new WhatYourRole(["558947013744525313", "executive_officer"]);
 
 module.exports = {
     WhatYourIgn
