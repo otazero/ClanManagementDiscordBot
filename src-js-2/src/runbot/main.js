283d9c6ef2aab39c3af4db89bde6bc06f3cc19d4
Dorayaki-Chan <zero.kotam@gmail.com>
2022-06-20 23:14:30 +0900

    monthly、daily処理書く

---
+++ src-js-2/src/runbot/main.js
@@ -0,0 +1,35 @@
+const {IntegrationApiRequest} = require('../api-requests/api-requests');
+const {OperationDatabase} = require('../operateDatabase/opratedatabase');
+/**
+ * 非アクティブ且つDIscord未参加抽出 総アクティブに加算+アクティブテーブルにinsert
+ */
+class Monthly{
+    constructor(){
+        this.#main();
+    }
+    #main(){
+        
+    }
+}
+
+/**
+ * 入退室者の更新
+ */
+class Daily{
+    constructor(){
+        this.#main();
+    }
+    async #main(){
+        // APIリクエストを飛ばして 既存データベースの内容もクラス化
+        let [discordusers, wotbusers, thunderusers] = await Promise.all([IntegrationApiRequest.requestDiscord(), IntegrationApiRequest.requestWotb(), IntegrationApiRequest.requestThunder()]);
+        // 重複してない人をDBにinsert(新規参加)
+        OperationDatabase.Insert(wotbusers, "Wotb");
+        // APIにいない且つDBの在籍フラグがTrueは退室者
+    }
+}
+
+const test= new Daily();
+
+module.exports = {
+    Monthly
+}
