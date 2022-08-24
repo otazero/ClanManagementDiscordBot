2a99e99db015bbf4bcfbfa12c53a8a5ac128a20d
Dorayaki-Chan <zero.kotam@gmail.com>
2022-08-25 00:59:50 +0900

    daily構築中

--- src-js-2/src/operateDatabase/opratedatabase.js
+++ src-js-2/src/operateDatabase/opratedatabase.js
@@ -29,10 +29,20 @@ class OperationDatabase{
                 const [result, gomi] = await mycon.query(`SELECT * FROM w_wotb_members`);
                 const oldWotbUserlist = result.map(user => {
                     let userclass = new WotbUser();
-                    userclass.id = result;
+                    
+                    userclass.id = user.w_user_id;
+                    userclass.ign = user.w_ign;
+                    //userclass.setrole = user.r_id;
+                    userclass.setEnter = user.w_enter_at;
+                    if(user.w_left_at){
+                        userclass.setLeft = user.w_left_at;
+                    }
+                    userclass.isflag = user.w_is_flag?true:false;
+
+                    return userclass;
                 });
-                
-                console.log(result[0].w_enter_at.getFullYear());
+                console.log(oldWotbUserlist);
+                //console.log(result[0].w_enter_at.getFullYear());
                 if( mycon ){
                     mycon.end();
                 }
@@ -56,8 +66,6 @@ class OperationDatabase{
     static async #InsertWotb(userclasslist){
         /* SELECT * FROM ~ */
         /* で決める */
-
-        
         let mycon = null;
         try {
             mycon = await mysql.createConnection(db_setting);
@@ -65,6 +73,8 @@ class OperationDatabase{
             console.log(e);
         }
         const enterUsers = [];
+        
+        // ここから
         for(const user of userclasslist){
             //const result_1 = await mycon.query(`SELECT w_user_id FROM w_wotb_members WHERE w_user_id = ${user.id}`);
             const result = await mycon.query(`INSERT INTO w_wotb_members(w_user_id, w_ign, r_id, w_enter_at) VALUES (${user.id}, '${user.ign}', ${user.role.main.id}, '${user.enter_at.getDateTime}') ON DUPLICATE KEY UPDATE {w_user_id:user.id, w_ign:user.ign, r_id:user.role.main.id, w_enter_at:user.enter_at.getDateTime}`);
@@ -75,10 +85,56 @@ class OperationDatabase{
         console.log(enterUsers);
         const result = await mycon.query(`INSERT INTO w_wotb_members SET ?`, {w_user_id:2, w_ign:"BlackTiger", r_id:4, w_enter_at:'2022-06-19 22:57:30'})
         console.log(result[0]);
+        // ここまで
+
+
         if( mycon ){
             mycon.end();
         }
         return enterUsers;
+    }
+
+    static async Daily(newusers){
+        let mycon = null;
+        try {
+            mycon = await mysql.createConnection(db_setting);
+        }catch(e){
+            console.log(e);
+        }
+
+        const [oldusers, gomi] = await mycon.query(`SELECT w_user_id, w_is_flag FROM w_wotb_members WHERE w_is_flag = true`);
+        let reentUserslist = [];
+        let entUserslist = [];
+        let leftUserslist = [];
+        oldusers.forEach(older => {
+            let isflag = false;
+            newusers.forEach(newer =>{
+                if(newer.id === older.w_user_id){
+                    isflag = true;
+                }
+            });
+            if(isflag){
+                /*　要:ユーザークラス化 */
+                leftUserslist.push(older);
+            }
+        });
+        newusers.forEach(newer => {
+            let isflag = false;
+            oldusers.forEach(older =>{
+                if(older.id === older.w_user_id){
+                    isflag = true;
+                }
+            });
+            if(isflag){
+                /*　要:ユーザークラス化 */
+                leftUserslist.push(older);
+            }
+        });
+
+
+        if( mycon ){
+            mycon.end();
+        }
     }
 }
 
--- src-js-2/src/runbot/main.js
+++ src-js-2/src/runbot/main.js
@@ -1,5 +1,6 @@
 const {IntegrationApiRequest} = require('../api-requests/api-requests');
 const {OperationDatabase} = require('../operateDatabase/opratedatabase');
+
 /**
  * 非アクティブ且つDIscord未参加抽出 総アクティブに加算+アクティブテーブルにinsert
  */
@@ -22,8 +23,10 @@ class Daily{
     async #main(){
         // APIリクエストを飛ばして 既存データベースの内容もクラス化
         let [discordusers, wotbusers, thunderusers] = await Promise.all([IntegrationApiRequest.requestDiscord(), IntegrationApiRequest.requestWotb(), IntegrationApiRequest.requestThunder()]);
-        // 重複してない人をDBにinsert(新規参加)
-        OperationDatabase.Insert(wotbusers, "Wotb");
+        // 重複してない人をDBにinsert(新規参加) & 参加者リストを返す
+        //OperationDatabase.Insert(wotbusers, "Wotb");
+        OperationDatabase.Daily(wotbusers);
+        //OperationDatabase.oldDatabaseInfo();
         // APIにいない且つDBの在籍フラグがTrueは退室者
     }
 }
