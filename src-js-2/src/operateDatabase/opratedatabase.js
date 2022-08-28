512f3384be71a0665438e8b78e6a7a3e7787b8ae
Dorayaki-Chan <zero.kotam@gmail.com>
2022-08-29 01:13:00 +0900

    要:退室時間記録

--- src-js-2/src/operateDatabase/opratedatabase.js
+++ src-js-2/src/operateDatabase/opratedatabase.js
@@ -19,15 +19,14 @@ class OperationDatabase{
      * @param {*} newusers APIの結果(ユーザークラス)を渡します 
      */
     static async Daily(wotbNewusers, thunderNewusers){
+        /** Wotb **/
         let mycon = null;
         try {
             mycon = await mysql.createConnection(db_setting);
         }catch(e){
             console.log(e);
         }
-
-        /** Wotb **/
-        const wotbDaily = (async(mycon, newusers)=>{
+        const wotbDaily = (async(newusers)=>{
             //データベースから存在フラグがTrueのだけ受け取ります
             const [oldusers, gomi] = await mycon.query(`SELECT * FROM w_wotb_members NATURAL INNER JOIN r_roles WHERE w_is_flag = true `);
             
@@ -72,12 +71,12 @@ class OperationDatabase{
                 });
                 return result;
             })(oldusers, newusers);
-            /*
-            console.log("退室");
+            
+            console.log("B退室");
             console.log(lefters);
-            console.log("入室");
+            console.log("B入室");
             console.log(enters);
-            */
+            
             //退室者に関するデータベース操作
             if(lefters.length){
                 (async(mycon)=> {
@@ -100,14 +99,74 @@ class OperationDatabase{
                     const [result, gomi] = await mycon.query(`INSERT INTO w_wotb_members(w_user_id, w_ign, r_id, w_enter_at, w_is_flag) VALUES ${q_text} AS new ON DUPLICATE KEY UPDATE w_is_flag = new.w_is_flag`);
                 })(mycon);
             }
+            if( mycon ){
+                mycon.end();
+            }
             return {lefters:lefters, enters:enters};
-        })(mycon, wotbNewusers);
+        })(wotbNewusers);
         
         /** WarThunder **/
-        
-        if( mycon ){
-            mycon.end();
-        }
+        const thunderDaily = (async(newusers)=>{
+            let mycon = null;
+            try {
+                mycon = await mysql.createConnection(db_setting);
+            }catch(e){
+                console.log(e);
+            }
+            //データベースから存在フラグがTrueのだけ受け取ります
+            const [oldusers, gomi] = await mycon.query(`SELECT * FROM t_wt_members NATURAL INNER JOIN r_roles WHERE t_is_flag = true `);
+            //退室者を考えます
+            const lefters = ((olds, news) => {
+                let result = [];
+                olds.forEach(older => {
+                    let isflag = false;
+                    news.forEach(newer =>{
+                        if(newer.id == older.t_user_id){
+                            isflag = true;
+                        }
+                    });
+                    if(!isflag){
+                        //ユーザークラス化
+                        let user = new ThunderUser();
+                        user.id = older.t_user_id;
+                        user.ign = older.t_ign;
+                        user.setrole = [older.r_dis_id];
+                        user.setEnter = older.t_enter_at;
+                        if(older.t_left_at){
+                            user.setLeft = older.t_left_at;
+                        }
+                        user.allactive = older.t_all_active;
+
+                        result.push(user);
+                    }
+                });
+                return result;
+            })(oldusers, newusers);
+            //入室者を考えます
+            const enters = ((olds, news) => {
+                let result = [];
+                news.forEach(newer => {
+                    let isflag = false;
+                    olds.forEach(older =>{
+                        if(newer.id == older.t_user_id){
+                            isflag = true;
+                        }
+                    });
+                    if(!isflag){
+                        result.push(newer);
+                    }
+                });
+                return result;
+            })(oldusers, newusers);
+            console.log("TH退室");
+            //console.log(lefters);
+            console.log("TH入室");
+            console.log(enters);
+            if( mycon ){
+                mycon.end();
+            }
+        })(thunderNewusers);
+
     }
 }
 
